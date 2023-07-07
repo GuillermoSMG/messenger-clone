5047c9de293b82c5f7d9ef8ee6a6e8634d6b3c3f
Guillermo San Martín <guille.seba0802@gmail.com>
2023-07-06 22:22:56 -0300

    conversations

---
+++ app/components/Box.tsx
@@ -0,0 +1,43 @@
+import { format } from 'date-fns';
+import { FullMessageType } from '../types';
+import clsx from 'clsx';
+
+interface BoxProps {
+  data: string;
+  lastMessage?: FullMessageType;
+  lastMessageText?: string;
+  hasSeen?: boolean;
+}
+
+const Box: React.FC<BoxProps> = ({
+  data,
+  lastMessage,
+  lastMessageText,
+  hasSeen,
+}) => {
+  return (
+    <div className='min-w-0 flex-1'>
+      <div className='focus:outline-none'>
+        <div className='flex justify-between items-center mb-1'>
+          <p className='text-sm font-medium text-gray-900'>{data}</p>
+          {lastMessage?.createdAt && (
+            <p className='text-xs text-gray-400 font-light'>
+              {format(new Date(lastMessage.createdAt), 'p')}
+            </p>
+          )}
+        </div>
+        {lastMessageText && (
+          <p
+            className={clsx(
+              'truncate text-sm',
+              hasSeen ? 'text-gray-500' : 'text-black font-medium'
+            )}
+          >
+            {lastMessageText}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Box;
