1e0dc5c34d71814ebb6542051c44cba786cec004
Guillermo San Martín <guille.seba0802@gmail.com>
2023-07-18 13:37:54 -0300

    styles

--- app/components/Box.tsx
+++ app/components/Box.tsx
@@ -22,7 +22,7 @@ const Box: React.FC<BoxProps> = ({
         <div className='flex justify-between items-center mb-1'>
           <p className='text-sm font-medium text-gray-900'>{data}</p>
           {lastMessage?.createdAt && (
-            <p className='text-xs text-gray-400 font-light'>
+            <p className='text-xs text-gray-400 font-light mr-2'>
               {format(new Date(lastMessage.createdAt), 'p')}
             </p>
           )}
